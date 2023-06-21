0c01ebe503eef90cb8460248768a425801d0e9fe
Oscar Eriksson <oscar.hillbo@gmail.com>
2023-06-21 12:48:22 +0000

    setup initial files for project based on Code institute test example as starting base. Models, templates and css. First admin and view done

--- workout_buddy/settings.py
+++ workout_buddy/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['workout-buddie-94d99620166c.herokuapp.com', 'localhost']
+# ALLOWED_HOSTS = ['workout-buddie-94d99620166c.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-oile995-project4-8c7qou1ejkp.ws-eu100.gitpod.io']
 
 
 # Application definition
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'workout',
 ]
 
