1e50a7006cb3124066aa94c6cb4a79726c1e2265
Oscar Eriksson <oscar.hillbo@gmail.com>
2023-06-26 13:55:59 +0000

    test debug false and x_frame for summernote

--- workout_buddy/settings.py
+++ workout_buddy/settings.py
@@ -30,11 +30,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['workout-buddie-94d99620166c.herokuapp.com', 'localhost','8000-oile995-project4-8c7qou1ejkp.ws-eu100.gitpod.io']
-# ALLOWED_HOSTS = ['8000-oile995-project4-8c7qou1ejkp.ws-eu100.gitpod.io']
 
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
 
